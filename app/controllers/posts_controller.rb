bc9d5e4b8c6f6b99429ba62d361ca0c8c0bdc410
Jennifer <palacios.jennifer@gmail.com>
2015-02-17 19:18:34 +0100

    updated post controller to allow posts to be created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,9 +12,11 @@ class PostsController < ApplicationController
   end
 
    def create
-      @topic = Topic.find(params[:topic_id])
-     @post = current_user.posts.build(post_params)
-     authorize @post
+    @topic = Topic.find(params[:topic_id])
+    @post = current_user.posts.build(post_params)
+    @post.topic = @topic
+    authorize @post
+     
      if @post.save
        flash[:notice] = "Post was saved."
        redirect_to [@topic, @post]
@@ -34,9 +36,10 @@ class PostsController < ApplicationController
       @topic = Topic.find(params[:topic_id])
      @post = Post.find(params[:id])
      authorize @post
+     
      if @post.update_attributes(post_params)
        flash[:notice] = "Post was updated."
-       redirect_to @post
+       redirect_to [@topic, @post]
      else
        flash[:error] = "There was an error saving the post. Please try again."
        render :edit
