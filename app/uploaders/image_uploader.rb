f6a71ae133cd1fb7bdc2eab25c5fc8d83b0e634a
Jennifer <palacios.jennifer@gmail.com>
2015-02-18 21:09:21 +0100

    fixed image sizes

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,7 +25,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
-  process :resize_to_fill => [200, 300]
+  process :resize_to_fill => [700, 400]
   
   # def scale(width, height)
   #   # do something
@@ -33,7 +33,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
    version :thumb do
-     process resize_to_fill: [100, 75]
+     process resize_to_fill: [700, 400]
    end
 
   # Add a white list of extensions which are allowed to be uploaded.
