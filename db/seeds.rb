44585e733773694e8827b3574fbe2eb6cbbf8123
Jennifer <palacios.jennifer@gmail.com>
2015-02-26 19:02:41 +0100

    spec tests passed

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,6 +39,7 @@ topics = Topic.all
    )
    # sets the created_at to a time within the past year
    post.update_attributes!(created_at: rand(10.minutes .. 1.year).ago)
+   post.create_vote
    post.update_rank
  end
  
