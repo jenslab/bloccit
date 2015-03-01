e98049f32b8d4aba35cf298f4cf452cf898ed77e
Jennifer <palacios.jennifer@gmail.com>
2015-03-01 12:43:54 +0100

    added destroying with ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,10 +25,13 @@
 
      if @comment.destroy
        flash[:notice] = "Comment was removed."
-       redirect_to [@post.topic, @post]
      else
        flash[:error] = "Comment couldn't be deleted. Try again."
-       redirect_to [@post.topic, @post]
+     end
+
+      respond_to do |format|
+       format.html
+       format.js
      end
    end
 
